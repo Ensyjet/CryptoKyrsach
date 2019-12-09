a525a4b268fabfe253018c77730830ad5c59cbfe
Ensyjet <49925891+Ensyjet@users.noreply.github.com>
2019-12-09 23:22:45 +0300

    Add files via upload

---
+++ Hash/Hash/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hash
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputText = "ЛЕГКОВ";
+            int p = 5;
+            int q = 19;
+            int n = p * q;
+            int H0 = 8;
+            int hash= NumberLetters(inputText,n,H0);
+            Console.WriteLine("Хеш-образ:" + hash);
+            Console.ReadKey();
+        }
+        private static int  NumberLetters(string inputText, int n, int H0)
+        {
+            var hash = H0;
+            char[] TextMas = inputText.ToCharArray();
+            for(int i=0; i < TextMas.Length; i++)
+            {
+                int DopTransformation = (int)TextMas[i]-848;
+                hash =(int) (Math.Pow(hash + DopTransformation,2)) % n;
+            }
+            return hash;
+        }
+    }
+}
